03d64649bd2b3d31571724eb8ecb704aea05842f
Sol Battaglia <solbattaglia@gmail.com>
2022-02-05 19:09:43 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Solbaa/web-dev
+
+go 1.17
+
+require github.com/gorilla/mux v1.8.0
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", r)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Holaaa")
+
+}
